1d27fca93a28d3e02bb61d30cbf2b1f2dd12c16b
EmiCon97 <derleuchtturm97@gmail.com>
2024-01-26 15:57:36 -0300

    chore: addede refresh expiration type

--- src/auth/config/refresh-expiration.ts
+++ src/auth/config/refresh-expiration.ts
@@ -1,4 +1,6 @@
-const refreshExpiration = {
+type RefreshExpiration = { asString: string; asNumber: number };
+
+const refreshExpiration: RefreshExpiration = {
   asString: '7d',
   asNumber: 1000 * 60 * 60 * 24 * 7,
 } as const;
